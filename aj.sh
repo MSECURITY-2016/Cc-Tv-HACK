61dc47a812878a19ec6c64fa93aa58949ba4d398
Mafuz-eng <73645261+Mafuz-eng@users.noreply.github.com>
2020-10-29 17:07:10 +0530

    Add files via upload

---
+++ aj.sh
@@ -0,0 +1,183 @@
+# A tool mafuz
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#-----------------
+# scripting starts
+clear
+echo '
+                
+                          _________
+                         |#########|
+                         |O#######O|
+                         |#########|
+                         |###---###|
+                         -----------
+                            |||||  
+                            |||||
+                ||||||||||||||||||||||||||||||
+                ||||||||||||||||||||||||||||||
+                       |###############|
+                       |###############|
+                       |#MAFUZ HACKERS#|
+                       |###############|
+                       |###############|
+                       |###############|
+                       |###############|
+                       -----------------
+                       |###|       |###|
+                       |###|       |###|
+                       |###|       |###|
+                       |###|       |###|
+                       |###|       |###|
+                       |###|       |###|
+                     |_______|   |_______|                                                          
+                                                                        
+'|lolcat
+echo " " 
+
+echo -e "$red                      ▶ Coded by$grn N17RO$red ◀$rset"
+echo -e "$red                 ⫸$ylo  🇲​​​​​ 🇷​​​​​  🇲​​​​​ 🇦​​​​​ 🇫​​​​​ 🇺​​​​​ 🇿 ⫷$rset"
+echo 
+# Functions
+echo -e "$red                   [Infect them with fun]$rset"
+echo " "
+echo " "
+echo -e "$grn              ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [1️⃣] Infect now"
+echo -e "$red                        ➡$cyan [2️⃣] Save now"
+echo -e "$red                        ➡️$cyan [3️⃣] Update script"
+echo -e "$red                        ➡$cyan [4️⃣] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -e "$cyan                   [[[$ylo Select any option$cyan]]]$rset"
+echo " "
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo " "
+echo -e "$red               You are going to$grn Infect$red Now$rset"
+echo " "
+echo -e "$grn                  Press$ylo ENTER$red to continue$rset"
+echo " "
+read hulke
+cd $HOME/Infect/lol
+echo " "
+echo -e "$ylo     To terminate the process click$red 't'$ylo or to continue click on$grn ENTER$rset" 
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                    Are you sure? Press$grn ENTER$red to exit$rset"
+read hulkee
+cd $HOME/Infect/
+bash infect.sh
+else
+bash sysup.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan            click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Infect/
+bash infect.sh ;;
+2)echo " "
+echo " "
+echo " "
+echo -e "$red                You are going to$red Save victim$rset "
+echo " "
+echo -e "$grn                  Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitro
+cd $HOME/Infect/lol
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                   Are you sure? Press$grn ENTER$red to exit$rset"
+read nitroo
+cd $HOME/Infect
+bash infect.sh
+else
+bash save.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan             click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Infect
+bash infect.sh ;;
+3)echo " "
+echo " "
+echo " "
+echo -e "$red             You are going to$grn Update the$red Script$rset "
+echo " "
+echo -e "$grn                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+read mon
+cd $HOME/Infect/lol
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                 Are you sure? Press$grn ENTER$red to exit$rset"
+read monn
+cd $HOME/Infect
+bash infect.sh
+else
+bash update.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan        click$grn ENTER$cyan to go back into infect  menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Infect
+bash infect.sh ;;
+4)
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+     ____  _  _  ____    ____  _  _  ___
+    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+     ) _ < \  /  )__)    ) _ < \  /  )__)
+    (____/ (__) (____)  (____/ (__) (____)...$cyan L.T.H HACKERS $rset"
+
+echo " "
+echo " "
+esac
+
+    © 2020 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+
